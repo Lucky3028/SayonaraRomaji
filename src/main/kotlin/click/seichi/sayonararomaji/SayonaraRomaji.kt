c36574a94ba2a92497fb74f53d3eb6842d5f8695
Lucky3028 <lucky3028.0@gmail.com>
2020-11-27 15:37:08 +0900

    Initial commit

---
+++ src/main/kotlin/click/seichi/sayonararomaji/SayonaraRomaji.kt
@@ -0,0 +1,13 @@
+package click.seichi.sayonararomaji
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SayonaraRomaji : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
