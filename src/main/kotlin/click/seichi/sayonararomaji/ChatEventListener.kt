57b317dab40a1ea19701127ec9a5edecaec73a58
Lucky3028 <lucky3028.0@gmail.com>
2020-11-27 16:24:16 +0900

    [Remove] dependence on Spigot

--- src/main/kotlin/click/seichi/sayonararomaji/ChatEventListener.kt
+++ src/main/kotlin/click/seichi/sayonararomaji/ChatEventListener.kt
@@ -6,17 +6,21 @@ import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.event.ChatEvent
 import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.event.EventHandler
-import org.bukkit.Bukkit
 
 class ChatEventListener : Listener {
     @EventHandler
     fun onPlayerChat(event: ChatEvent) {
         if (event.isCancelled || event.isCommand || event.sender !is ProxiedPlayer) return
 
-        ProxyServer.getInstance().scheduler.runAsync(PLUGIN) {
-            Bukkit.getServer().logger.info("Player chatted: ${event.message}")
+        val serverInstance = ProxyServer.getInstance() ?: return
+        serverInstance.scheduler.runAsync(PLUGIN) {
+            serverInstance.logger.info("Player chatted: ${event.message}")
         }
 
         event.isCancelled = true
+    }
+
+    private fun japanize() {
+
     }
 }
